5d96752e2bc5713d5092ed309998615b3ee9f619
Ni Le <ni.le13062013@gmail.com>
2026-04-15 09:54:44 +0200

    entity

---
+++ src/main/java/de/htw_berlin/mytodolist/FootworkSession.java
@@ -0,0 +1,37 @@
+package de.htw_berlin.mytodolist;
+
+public class FootworkSession {
+    private int workTime;
+    private int restTime;
+    private int round;
+
+
+    public FootworkSession (int workTime, int restTime, int  round){
+    this.workTime = workTime;
+    this.restTime = restTime;
+    this.round = round;
+}
+
+    public void setWorkTime(int workTime) {
+        this.workTime = workTime;
+    }
+
+    public int getWorkTime() {
+        return workTime;
+    }
+    public int getRestTime() {
+        return restTime;
+    }
+
+    public void setRestTime(int restTime) {
+        this.restTime = restTime;
+    }
+    public int getRound() {
+        return round;
+    }
+
+    public void setRound(int round) {
+        this.round = round;
+    }
+
+}
