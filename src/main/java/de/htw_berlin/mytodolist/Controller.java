fc0090d2baa93121a24f7bdc89a4bc21690bec15
Ni Le <ni.le13062013@gmail.com>
2026-04-15 09:59:38 +0200

    entity

--- src/main/java/de/htw_berlin/mytodolist/Controller.java
+++ src/main/java/de/htw_berlin/mytodolist/Controller.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-@GetMapping
-public String test() {
-    return "Hello World";
+@GetMapping("/session")
+    public FootworkSession getSession(){
+    return new FootworkSession (40, 20, 3);
 }
 
 }
